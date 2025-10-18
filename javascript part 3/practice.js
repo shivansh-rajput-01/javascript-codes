41a97b8fc473befeb090acea4e4bbed2cdc89cdd
shivansh-rajput-01 <shivanshr579@gmail.com>
2025-10-18 22:22:51 +0530

    used splice method to replace and add values

---
+++ javascript part 3/practice.js
@@ -0,0 +1,11 @@
+// create an array ['january','july','march','august']
+// covert it to ['july','june','march','august'] using splice() method
+let arr = ['january','july','march','august'];
+// arr.splice(0,1);
+// arr.splice(1,0,'june');
+// console.log(arr);
+
+//or
+
+arr.splice(0,2,'july','june');
+console.log(arr);
